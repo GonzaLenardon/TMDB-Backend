933338ecf399a7b58b002b8911124e867a3f1441
GonzaLenardon <gonlenardon@gmail.com>
2023-08-03 20:34:56 -0300

    generate and validate token

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,7 @@
 const { User } = require("../models");
+const { generateToken, validateToken } = require("../config/token");
 
-const getUser = (req, res) => {
+const UserId = (req, res) => {
   const { id } = req.params;
   User.findOne({ where: { id } }).then((user) => res.status(200).send(user));
 };
@@ -23,12 +24,32 @@ const addUser = (req, res) => {
 const login = (req, res) => {
   const { email, pass } = req.body;
   User.findOne({ where: { email } }).then((user) => {
-    if (!user) return res.status(401).send("email NO registrador");
+    if (!user)
+      return res.status(401).send(`la cuenta  ${email} no esta registrador`);
 
     user.validatePass(pass).then((isValid) => {
-      if (!isValid) return res.status(401).send("email NO registrador");
+      if (!isValid) return res.status(401).send("wrong password");
+
+      const payload = {
+        name: user.name,
+        email: user.email,
+      };
+
+      const token = generateToken(payload);
+      res.cookie("Token ", token);
+      res.send(payload);
     });
   });
+};
+
+const getUser = (req, res) => {
+  const token = req.cookies.Token;
+  if (!token) return res.status(401).send("Session expirada");
+
+  const { user } = validateToken(token);
+  if (!user) return res.status(401);
+
+  res.send(user);
 };
 
 module.exports = { addUser, getUser, login };
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controllersUser = require("../controllers/User");
 
-router.get("/user/:id", controllersUser.getUser);
+//router.get("/user/:id", controllersUser.UserId);
 
-router.post("/user", controllersUser.addUser);
+router.post("/user/add", controllersUser.addUser);
 
 router.post("/user/login", controllersUser.login);
 
-/* router.get("/user", (req, res) => {
-  User.findAll().then((res) => res.send(res));
-});
-
-
-
-router.delete("/user/:id", (req, res) => {
-  const { id } = req.params;
- 
-  User.destroy({ where: { id } }).then((resp) => res.send("User deleted"));
-});
- */
+router.get("/user/me", controllersUser.getUser);
 
 module.exports = router;
