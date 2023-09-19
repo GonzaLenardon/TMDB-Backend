a45a39294addff9e49929fe77c04d21c189886d2
GonzaLenardon <gonlenardon@gmail.com>
2023-09-19 10:18:28 -0300

    favorites view controller created

--- controllers/Favorites.js
+++ controllers/Favorites.js
@@ -6,43 +6,36 @@ const addFavorite = (req, res) => {
 
   User.findOne({ where: { email } }).then((user) => {
     if (user) {
-      Favorites.create({ Movie })
-        .then((favorite) => {
-          favorite.setUser(user);
-          res.status(201).send(favorite);
-        })
-        .catch((erro) => console.log(erro));
+      Favorites.findOne({
+        where: { userId: user.id, Movie: Movie },
+      }).then((rex) => {
+        if (!rex) {
+          Favorites.create({ Movie })
+            .then((favorite) => {
+              favorite.setUser(user);
+              res.send(201);
+              //res.status(201).send(favorite);
+              console.log("agregamos favoritos");
+            })
+            .catch((erro) => console.log("Dio un errorororor", erro));
+        } else {
+          res.send(204);
+          console.log("ya existe este movie en favoritos");
+        }
+      });
     }
   });
 };
-
-/* const delFavorite = (req, res) => {
-  const { email } = req.body;
-  const Movie = req.params.id;
-
- User.findOne({ where: { email } }).then((user) => {
-    if (user) {
-      Favorites.destroy({ where:   })
-        .then((favorite) => {
-          favorite.setUser(user);
-          res.status(201).send(favorite);
-        })
-        .catch((erro) => console.log(erro));
-    }
-  });
-}; */
 
 const allFavorites = (req, res) => {
   const email = req.params.email;
   User.findOne({ where: { email } })
     .then((result) => {
       const userId = result.id;
-      console.log("este es usuiario", userId);
 
       Favorites.findAll({ where: { userId } })
         .then((movies) => {
           res.status(200).send(movies);
-          console.log("adfadsfadsfadfsadff", movies);
         })
         .catch((error) => console.log(error));
     })
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 
-db.sync({ force: true })
+db.sync({ force: false })
 
   .then(() => {
     app.listen(app.get("port"), () =>
