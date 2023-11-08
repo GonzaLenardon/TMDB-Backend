1114398bcfdd946656659ae8feece02dacd057de
GonzaLenardon <gonlenardon@gmail.com>
2023-11-08 09:07:31 -0300

    deploy config

--- models/_db.js
+++ models/_db.js
@@ -1,9 +1,21 @@
 const Sequelize = require("sequelize");
 
+/* config local  
 const db = new Sequelize("tmdb", "postgres", "postgres", {
   host: "localhost",
   dialect: "postgres",
   logging: false,
 });
+ */
+const db = new Sequelize(
+  "postgrestmdb",
+  "postgrestmdb_user",
+  "D6BruXxaRDMpd7URxQtNT0wbE13xkJHq",
+  {
+    host: "dpg-cl54l2al7jac73cc3nlg-a",
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = db;
